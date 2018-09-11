2e26a6b2e9850df955fe1bf0e21c7691a7bb8b16
zhixi0912 <985840977@qq.com>
2018-09-11 16:00:29 +0800

    列表冲突

--- src/page/ContentList.vue
+++ src/page/ContentList.vue
@@ -35,11 +35,6 @@
     methods: {
       getData () {
         this.$api.get('topic/' + this.id, null, res => {
-<<<<<<< HEAD
-
-=======
-          console.log(res);
->>>>>>> company
           this.contentList = res.data
           console.log(this.contentList.author);
           console.log(this.contentList.replies);
