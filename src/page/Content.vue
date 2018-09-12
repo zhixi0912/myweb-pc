25042bcb445a5ce1467575573e8b5919669dc73a
zhixi0912 <985840977@qq.com>
2018-09-12 20:28:07 +0800

    动画

--- src/page/Content.vue
+++ src/page/Content.vue
@@ -13,39 +13,39 @@
       <div class="section section-1">
         <el-header class="animated faedOutUp"><myHeader></myHeader></el-header>
         <div class="main web-w clearfix">
-            <h3 id="page1-tiele-h1" class="text-center" >标题1</h3>
+            <h3 id="page1-tiele-h1" class="text-center" :class="{animated:page1Title,fadeInUpBig:page1Title}">标题1</h3>
           <el-row :gutter="20">
             <el-col :span="12">
-              <div class="grid-content bg-purple bg-purple-left" id="page1-left" >left</div>
+              <div class="grid-content bg-purple bg-purple-left" id="page1-left" :class="{animated:page1l,rotateInDownLeft :page1l}">left</div>
             </el-col>
             <el-col :span="12">
-              <div class="grid-content bg-purple bg-purple-right" id="page1-right" >right</div>
+              <div class="grid-content bg-purple bg-purple-right" id="page1-right"  :class="{animated:page1r,rotateInDownRight :page1r}">right</div>
             </el-col>
           </el-row>
         </div>
       </div>
       <div class="section section-2">
         <div class="main web-w clearfix">
-          <h3 id="page2-tiele-h1" class="text-center">标题2</h3>
+          <h3 id="page2-tiele-h1" class="text-center" :class="{animated:page2Title,fadeInUpBig:page2Title}">标题2</h3>
           <el-row :gutter="20">
             <el-col :span="12">
-              <div class="grid-content bg-purple bg-purple-left" id="page2-left" @click='fade'>left</div>
+              <div class="grid-content bg-purple bg-purple-left" id="page2-left" :class="{animated:page2l,rotateInDownLeft:page2l}">left</div>
             </el-col>
             <el-col :span="12">
-              <div class="grid-content bg-purple bg-purple-right" id="page2-right" :class="{bgBlue:pages,bgBlue:pages}">right</div>
+              <div class="grid-content bg-purple bg-purple-right" id="page2-right" :class="{animated:page2r,rotateInDownRight :page2r}">right</div>
             </el-col>
           </el-row>
         </div>
       </div>
       <div class="section section-3">
         <div class="main web-w clearfix">
-          <h3 id="page3-tiele-h1" class="text-center">标题3</h3>
+          <h3 id="page3-tiele-h1" class="text-center" :class="{animated:page3Title,fadeInUpBig:page3Title}">标题3</h3>
           <el-row :gutter="20">
             <el-col :span="12">
-              <div class="grid-content bg-purple bg-purple-left" id="page3-left">left</div>
+              <div class="grid-content bg-purple bg-purple-left" id="page3-left" :class="{animated:page3l,rotateInDownLeft:page3l}">left</div>
             </el-col>
             <el-col :span="12">
-              <div class="grid-content bg-purple bg-purple-right" id="page3-right">right</div>
+              <div class="grid-content bg-purple bg-purple-right" id="page3-right"  :class="{animated:page3r,rotateInDownRight :page3r}">right</div>
             </el-col>
           </el-row>
         </div>
@@ -62,28 +62,33 @@
         options: {
           afterLoad: this.afterLoad,
         },
-        pages: false,
+        page1Title: false,
+        page2Title: false,
+        page3Title: false,
+        page1r: false,
+        page2r: false,
+        page3r: false,
+        page1l: false,
+        page2l: false,
+        page3l: false,
       }
     },
     methods: {
       afterLoad(anchorLink,index ) {
         var pageA = index.index+1;
           if(pageA==1){
-            console.log("aa")
-
-            // document.getElementById('page1-tiele-h1').setAttribute('class','flip')
-            // document.getElementById('page1-tiele-h1').setAttribute('class','animated')
+            this.page1Title = true
+            this.page1l = true
+            this.page1r = true
           }else if(pageA==2){
-            console.log(pageA);
-            this.pages = true
-            // document.getElementById('page2-tiele-h1').setAttribute('class','flip')
-            // document.getElementById('page2-tiele-h1').setAttribute('class','animated')
-        }
-      }
-      ,
-      fade:function(){
-        document.getElementById('page2-left').setAttribute('class','flip')
-        // document.getElementById('page2-left').setAttribute('class','animated')
+            this.page2Title = true
+            this.page2l = true
+            this.page2r = true
+        }else if(pageA==3){
+            this.page3Title = true
+            this.page3l = true
+            this.page3r = true
+          }
       }
       ,addSection: function(e) {
         e.preventDefault();
