858ddebf0c5db1eb491c38b35f5e023fe96367fb
zhixi0912 <382893000@qq.com>
2019-04-09 11:32:07 +0800

    docs2

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ module.exports = {
     // assetsRoot: path.resolve(__dirname, '../dist'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
